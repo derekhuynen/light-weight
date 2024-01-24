74f62ece927074ef879584f16e910dfe8dd937d8
Derek Huynen <derek.huynen@gmail.com>
2024-01-23 23:57:45 -0800

    model example

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,2 +1,15 @@
-export type User = { id: string; name: string; email: string };
-export type UserPost = Omit<User, 'id'>;
+//Utility type to make dynamic types easier to read
+//eslint-disable-next-line
+export type Prettify<T> = { [K in keyof T]: T[K] } & {};
+
+type UserApiResponse = {
+	id: string;
+	name: string;
+	email: string;
+};
+
+type User = Prettify<UserApiResponse>;
+type UserPost = Omit<User, 'id'>;
+type UserPut = UserPost;
+
+export type { User, UserPost, UserPut };
